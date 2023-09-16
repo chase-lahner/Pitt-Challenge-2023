056b5d7a04302db67b0207f92a38674dce12eded
rcm71 <rcm71@pitt.edu>
2023-09-16 11:54:15 -0400

    colortest

--- testcolordetection.py
+++ testcolordetection.py
@@ -1,10 +1,10 @@
 import cv2
 import numpy as np 
-input_img = cv2.imread("redpillTest.JPG")
+input_img = cv2.imread("")
 img = cv2.resize(input_img, (640, 480))
 #cv2.imshow('image', img)
 input_img_cpy = img.copy()
-cv2.waitKey(0)
+
 
 
 # Convert RGB/ BGR to HSV color space
@@ -18,8 +18,8 @@ upper_red = np.array([10, 255, 255])
 mask_red = cv2.inRange(hsv, lower_red, upper_red)
  
 # Display filtered image
-#cv2.imshow('mask_red', mask_red)
-cv2.waitKey(0)
+cv2.imshow('mask_red', mask_red)
+
 
 # find contours in the red mask
 contours_red, _ = cv2.findContours(mask_red, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
