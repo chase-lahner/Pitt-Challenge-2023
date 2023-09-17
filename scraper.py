345e6d315d99928704ba040e20ca09306f44a7e5
lampealex888 <lampealex888@gmail.com>
2023-09-17 07:54:23 -0400

    scraper good

--- scraper.py
+++ scraper.py
@@ -1,57 +1,59 @@
 # Import the beautifulsoup 
 # and request libraries of python.
 import requests
-import bs4
+from bs4 import BeautifulSoup
+import re
+from collections import Counter
 
-def get_headlines(text):
-        # Combine two strings with default google search URL
-        # 'https://google.com/search?q=' and
-        # our customized search keyword parameter.
-        # Concatenate them
-        url = 'https://google.com/search?q=' + text
+def find_most_common_word(strings):
+    # Define a list of words and patterns to exclude
+    excluded_words = ["pill", "drug", "images", "tablet", "rectangle", ".com"]
+    excluded_pattern = re.compile(r'[.,!?():;"]')
 
-        # Fetch the URL data using requests.get(url),
-        # store it in a variable, request_result.
-        request_result=requests.get( url )
-        
-        # Creating soup from the fetched request
-        soup = bs4.BeautifulSoup(request_result.text,
-                                "html.parser")
-        # soup.find.all( h3 ) to grab 
-        # all major headings of our search result,
-        heading_object=soup.find_all( 'h3' )
-        
-        # Iterate through the object 
-        # and add it to the list
-        headlines = []
-        for info in heading_object:
-            headlines.append(info.getText())
+    # Create a list to store cleaned words
+    cleaned_words = []
 
-        return headlines
+    # Split each string into words, clean them, and add to the list
+    for string in strings:
+        words = string.split()
+        cleaned_words.extend([word.lower() for word in words if word.lower() not in excluded_words])
 
-# pittsburgh_headlines = get_headlines("pittsburgh")
-# for text in pittsburgh_headlines:
-#     print(text)
+    # Remove punctuation and excluded patterns from cleaned words
+    cleaned_words = [re.sub(excluded_pattern, '', word) for word in cleaned_words]
 
-def get_drugs_for_imprint(imprint):
-    url = f"https://dailymed.nlm.nih.gov/dailymed/search.cfm?labeltype=all&query={imprint}"
+    # Filter out words with less than 5 letters
+    cleaned_words = [word for word in cleaned_words if len(word) >= 5]
+
+    # Count the occurrences of each cleaned word
+    word_counts = Counter(cleaned_words)
+
+    # Find the most common word
+    most_common_word = word_counts.most_common(5)
+
+    if most_common_word:
+        return most_common_word
+    else:
+        return None
+    
+def search_drug_by_imprint(imprint):
+    search_query = "What drug is the pill imprint:" + imprint
+    url = f"https://www.google.com/search?q={search_query}"
     
     try:
-        response = requests.get(url)
-        response.raise_for_status()
+        request_result = requests.get(url)
+        # response.raise_for_status()
         
-        soup = BeautifulSoup(response.content, "html.parser")
+        soup = BeautifulSoup(request_result.text,
+                                "html.parser")
         
-        # Find the drug information section
-        drug_info_section = soup.find("div", class_="section-text")
-        
-        # Extract drug names
-        drug_names = []
-        for item in drug_info_section.find_all("strong"):
-            drug_names.append(item.get_text().strip())
+        heading_object = soup.find_all( 'h3' )
+
+        search_results = []
+        for result in heading_object:
+            search_results.append(result.getText())
         
         # Return up to 5 drug names
-        return drug_names[:5]
+        return find_most_common_word(search_results)
     
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
@@ -59,11 +61,48 @@ def get_drugs_for_imprint(imprint):
 
 if __name__ == "__main__":
     imprint = input("Enter the pill imprint: ")
-    drugs = get_drugs_for_imprint(imprint)
+    search_results = search_drug_by_imprint(imprint)
     
-    if drugs:
-        print("Possible drugs:")
-        for i, drug in enumerate(drugs, start=1):
-            print(f"{i}. {drug}")
+    if search_results:
+        for result in search_results:
+            print(result)
+
+    #     print("Possible drugs:")
+    #     for result in search_results:
+    #         print(result)
+    #     most_common = find_most_common_word(search_results)
+    #     print("Most common:")
+    #     print(most_common)
+    
     else:
         print("No drugs found for the given imprint.")
+
+# def get_headlines(text):
+#         # Combine two strings with default google search URL
+#         # 'https://google.com/search?q=' and
+#         # our customized search keyword parameter.
+#         # Concatenate them
+#         url = 'https://google.com/search?q=' + text
+
+#         # Fetch the URL data using requests.get(url),
+#         # store it in a variable, request_result.
+#         request_result=requests.get( url )
+        
+#         # Creating soup from the fetched request
+#         soup = bs4.BeautifulSoup(request_result.text,
+#                                 "html.parser")
+#         # soup.find.all( h3 ) to grab 
+#         # all major headings of our search result,
+#         heading_object=soup.find_all( 'h3' )
+        
+#         # Iterate through the object 
+#         # and add it to the list
+#         headlines = []
+#         for info in heading_object:
+#             headlines.append(info.getText())
+
+#         return headlines
+
+# pittsburgh_headlines = get_headlines("pittsburgh")
+# for text in pittsburgh_headlines:
+#     print(text)
