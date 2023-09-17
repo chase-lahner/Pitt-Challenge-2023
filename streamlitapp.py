66e5b003c8f4ab9e53df21d4b03139412fc4b4e3
rcm71 <rcm71@pitt.edu>
2023-09-16 23:43:25 -0400

    sdf

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,7 +1,14 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import colorAndShape as cs
 DATE_COLUMN = 'date/time'
 
 st.title('Drug Wizard')
-img = st.file_uploader("Upload ur file pls thx")
+img = st.file_uploader("file plesas")
+if img is not None:
+    x = open("/tmp/incoming/onefile", "wb+")
+    x.write(img.read())
+    x.close()
+    cs.identify("/tmp/incoming/onefile")
+    st.image('drugWizard.webp', caption = None, width=250)
